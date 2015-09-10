b490a72691410352bd56781f7b6e72babcd9c265
winnux <lihaibo@126.com>
2015-09-10 14:53:47 +0800

    Update Config.java
    modify token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "lihaibozone";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "1345d1c0891adf2512f49a2bb0a92068"; 
 	
 }
